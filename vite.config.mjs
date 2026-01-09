5eb8355574b9a5a43c631d042c150bfd259d47b5
willie-ysys <willie07180@gmail.com>
2026-01-09 19:02:31 +0800

    Fix Vite base for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // ✅ Vercel 一定要是 /
-})
+  base: "/", // ✅ Vercel 一定要是 /
+});
