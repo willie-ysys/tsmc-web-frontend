35071f2cd21b459e5b297ba0101cc115c7b55bd0
willie-ysys <willie07180@gmail.com>
2026-01-09 18:59:55 +0800

    Fix Vite base path for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 這裡請先用你「準備建立的前端公開 repo 名稱」
-// 我建議 repo 名稱就叫：tsmc-web-frontend
-// 之後如果 repo 名稱不同，再回來改這一行即可
 export default defineConfig({
   plugins: [react()],
-  base: '/tsmc-web-frontend/',
+  base: '/',   // ✅ Vercel 一定要是 /
 })
